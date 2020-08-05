4da5e94dc9752fe4d4dba1d88e6a77dc7accdffa
Kennuy <ken006@uscd.edu>
2020-08-05 16:14:40 -0700

    Brainstormed a workflow for script

--- webcrawler.py
+++ webcrawler.py
@@ -1,1 +1,8 @@
-print('hello world!')
+"""
+Libraries to use potentially: Scrapy, Selenium, Pandas, NumPy
+
+Workflow: Will send a get request to a specified user or search for that user using some API.
+Scrapy will then parse the data using HTML magic and parse all posts that have the #blkbox or
+#blkboxmusic hashtag. We can enhance this by using computer vision to allow the script to
+recognize if the BLKBox watermark shows in a post. 
+"""
