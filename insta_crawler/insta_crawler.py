b6480384005ab9ba51c845bd0ad94a653bec1bed
jn1118 <jix209@ucsd.edu>
2020-08-11 12:55:44 -0700

    remove extra

--- insta_crawler/insta_crawler.py
+++ insta_crawler/insta_crawler.py
@@ -32,7 +32,6 @@ class InstaBot:
 
       # Get the number of likes for current post
       parent_view.click()
-      #self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[2]/div/span')[0].click()
       parent_likes = self.driver.find_element_by_css_selector('div.vJRqr')
       likes = parent_likes.find_element_by_tag_name('span').text
       print('Likes: ' + str(likes))
@@ -46,27 +45,13 @@ class InstaBot:
       # Sleep a bit before moving onto next url
       time.sleep(3)
     
-    # Zip up info and store into a list
-    #self.zipped_data = zip(self.urls, self.comments, self.views, self.likes, self.dates)
       
 
 # Populate url list
 urls = ['https://www.instagram.com/p/CB1XK-BH5ez/', 'https://www.instagram.com/p/B9y7pTAAW0n/',
         'https://www.instagram.com/p/CDuO-OHjeej/']
 
-# Run the bot
-
-
-# Upload bot's zipped data into csv file
-# fields = ['URL', 'Comments', 'Views', 'Likes', 'Date']
-# filename = 'stats.csv'
-# with open(filename, 'w') as csvfile:
-#   csvwriter = csv.writer(csvfile)
-
-#   for data in test.zipped_data:
-#     csvwriter.writerow(fields)
-#     csvwriter.writerows(data)
-
+#make csv
 test = InstaBot(urls)
 test.driver.close()
 fields = ['URL', 'Comments', 'Views', 'Likes', 'Date']
