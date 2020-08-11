2495f151cd4e14f9b17987b5c361f7cc81c9858a
Kennuy <ken006@uscd.edu>
2020-08-11 11:53:13 -0700

    Got a demo for Twitter crawler

--- twitter_crawler/twitter_crawler.py
+++ twitter_crawler/twitter_crawler.py
@@ -10,3 +10,38 @@ class TwitterBot:
     self.views = []
     self.likes = []
     self.dates = []
+
+    # Automate for every link in url
+    for url in urls:
+      # Get url
+      self.driver.get(url)
+      time.sleep(5)
+      print('\nURL: ' + str(url))
+
+      # Get the number of comments for current post
+      # No comments found in Twitter posts
+
+
+      # Get the number of views for current post
+      # Can't see views for Twitter posts
+
+      # Get the number of likes for current post
+      likes = self.driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/section/div/div/div[1]/div/div/article/div/div/div/div[3]/div[4]/div/div[2]/div/a/div/span/span').text
+      print('Likes: ' + str(likes))
+      self.likes.append(likes)
+
+      # Get the date of current post
+      date = self.driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/section/div/div/div[1]/div/div/article/div/div/div/div[3]/div[3]/div/div[1]/span[1]/span').text
+      print('Date posted: ' + str(date))
+      self.dates.append(date)
+
+      # Sleep a bit before moving onto next url
+      time.sleep(3)
+
+    # Zip up info and store into a list
+
+urls = ['https://twitter.com/BLKBOXapp/status/1286051479946702851', 'https://twitter.com/BLKBOXapp/status/1273655517823541249']
+
+test = TwitterBot(urls)
+
+test.driver.close()
