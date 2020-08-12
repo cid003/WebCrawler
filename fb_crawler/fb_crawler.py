da80274e1250d1b43f85fc64097978df46f08369
jn1118 <jix209@ucsd.edu>
2020-08-12 12:02:14 -0700

    fb crawler finished

--- fb_crawler/fb_crawler.py
+++ fb_crawler/fb_crawler.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 import csv
+from selenium.common.exceptions import NoSuchElementException
 
 class InstaBot:
   def __init__(self, urls): # no need for user info
@@ -16,23 +17,12 @@ class InstaBot:
         self.driver.get(url)
         time.sleep(5)      
         print('\nURL: ' + str(url))
-        # selenium.getEval("scrollBy(0, 400)")
-        #self.driver.execute_script("window.scrollTo(0,500)") 
-        
-        # Get the number of comments for current post
-        #   comments_list = self.driver.find_element_by_css_selector('ul.XQXOT.pXf-y')
-        #   num_comments = len(comments_list.find_elements_by_css_selector('ul.Mr508'))
-        #   print('Comments: ' + str(num_comments))
-        #   self.comments.append(num_comments)
-
-        # Get the number of views for current post
-        #   parent_view = self.driver.find_element_by_css_selector('span.vcOH2')
-        #   views = parent_view.find_element_by_tag_name('span').text
-        #   print('Views: ' + str(views))
-        #   self.views.append(views)
-        comments = self.driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[2]/form/div/div[2]/div[1]/div/div[3]/span[1]/a').text
-        self.comments.append(comments)
-        print(comments)
+        try:
+            comment=self.driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[2]/form/div/div[2]/div[1]/div/div[3]/span[1]/a').text.split(' ')[0]
+        except NoSuchElementException:
+            comment = 0
+        self.comments.append(str(comment))
+        print(comment)
         
         # Get the date of current post
         date = self.driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[1]/div[3]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/span[1]/span/a/abbr/span').text
@@ -45,21 +35,23 @@ class InstaBot:
         self.likes.append(likes)
         
         # get the number of views
-        self.driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[3]/div/div[1]/div[1]/div/div/div/div/div[1]/video').click()
-        #print('hi')
+        self.driver.find_elements_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[1]/div[3]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/span[1]/span/a/abbr/span')[0].click()
+        # can change based on internet condition
+        time.sleep(5)
         #view=self.driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/span[1]')
-        view = self.driver.find_elements_by_class_name('_44bh')
-        self.views.append(view)
-        print(view)
+        view = self.driver.find_elements_by_class_name('fcg')
+        a = [i.text for i in view]
+        singleview = str(a[7]).split(' ')[0]
+        print(singleview)
+        self.views.append(singleview)
+        
         
         # Sleep a bit before moving onto next url
         time.sleep(3)
-    
-      
 
 # Populate url list
-urls = ['https://www.facebook.com/lovexstereo/videos/855000578361086/']
-#https://www.facebook.com/lovexstereo/videos/217040609601399/
+urls = ['https://www.facebook.com/lovexstereo/videos/855000578361086/','https://www.facebook.com/lovexstereo/videos/217040609601399/']
+
 
 #make csv
 test = InstaBot(urls)
